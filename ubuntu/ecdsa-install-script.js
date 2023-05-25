a1719572de41cf31c0dd52e38ac6bc61d0e04466
Songonha <105191398+songonha@users.noreply.github.com>
2023-05-25 10:54:07 +0700

    Update ecdsa-install-script.js

--- ubuntu/ecdsa-install-script.js
+++ ubuntu/ecdsa-install-script.js
@@ -6,4 +6,29 @@
 => sudo apt-get install -y nodejs
 => node -v (v18.16.0)
 
-
+3/ Nginx reverse for Node App
+=> cd /etc/nginx/sites-available
+=> sudo nano ecdsa.saigontoken.com
+  (Copy this code to server)
+=============================
+sudo nano /etc/nginx/sites-available/ecdsa.saigontoken.com
+server {
+  listen 80;
+  listen [::]:80;
+  
+  root /var/www/ecdsa.saigontoken.com/html
+  index index.html index.htm index.nginx-debian.html
+  
+  server_name ecdsa.saigontoken.com
+  
+  location / {
+    proxy_pass http://localhost:5173;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+    
+  }
+}
+=========================
