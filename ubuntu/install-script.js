40c3f2fd6a5e86f79cb9dc3992138a3c58e2ab07
Songonha <105191398+songonha@users.noreply.github.com>
2023-05-25 08:38:43 +0700

    Create install-script.js

---
+++ ubuntu/install-script.js
@@ -0,0 +1,9 @@
+1/ Install Ngins
+=> sudo apt install nginx
+
+2/ Install Nodejs 18
+=> curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+=> sudo apt-get install -y nodejs
+=> node -v (v18.16.0)
+
+
